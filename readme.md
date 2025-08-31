e6f846ed71c98ff752b279549d97873d96092641
leluza <adelina.mayol@gmail.com>
2025-08-31 03:47:07 -0300

    redefinicion del readme con proposito + formato y contenido en cv

--- readme.md
+++ readme.md
@@ -1,8 +1,53 @@
-Pasos para levantar este Front
 
-0- nvm use 16
-1- npm install
-2- npm run start
-3- abrir http://localhost:3000
+# 🚀 Nombre del Proyecto
 
-los datos seran facilitados al levantar el back
+ *"Aplicación web desarrollada con React que permite gestionar tareas de manera sencilla y rápida."*
+
+---
+
+## 📸 Capturas de pantalla (opcional)
+![Demo](ruta/a/tu/imagen.png)
+
+---
+
+## 🛠️ Tecnologías utilizadas
+- [React](https://react.dev/)
+- [JavaScript ES6+](https://developer.mozilla.org/es/docs/Web/JavaScript)
+- [Node.js](https://nodejs.org/) & npm
+- [Fetch] para llamadas a API
+- [React Router] para navegación
+- [Ionic] para estilos
+
+---
+
+## 📂 Estructura del proyecto
+/project-root
+├── /public
+├── /src
+│ ├── /components
+│ ├── /images
+│ ├── /pages
+│ ├── /theme
+│ ├── App.js
+│ └── index.js
+├── package.json
+├── Dockerfile
+└── README.md
+
+
+---
+
+## ⚙️ Instalación y uso
+
+Clona el repositorio, cambiar version de maven e instala las dependencias:
+
+```bash
+git clone https://github.com/leluza/AppFullStackParaPortfolioFRONT
+cd frontend
+nvm use 16
+npm install
+npm run start
+abrir http://localhost:3000
+y acceder a la opcion del menu "CV"
+
+aclaracion: los datos de empleados, proveedores y clienes, seran facilitados al levantar el serviico backend unicamente
--- src/pages/cv/Cv.tsx
+++ src/pages/cv/Cv.tsx
@@ -16,110 +16,201 @@ const CV: React.FC = (props: any) => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>{"ADELINA MAYOL    --    FullStack Developer"}</IonTitle>
+
+          <IonCardTitle className="cv-card-title-header">
+            Adelina Mayol
+          </IonCardTitle>
+          <IonCardSubtitle className="cv-card-subtitle-header">
+            Desarrolladora Full Stack
+          </IonCardSubtitle>
+          <div className="cv-card-contact-header">
+            📍 Córdoba, Argentina · ✉️ adelina.mayol@gmail.com · 🔗
+            <a href="https://linkedin.com/in/adelina-mayol" target="_blank" rel="noopener noreferrer">
+              linkedin.com/in/adelina-mayol
+            </a>
+          </div>
+
         </IonToolbar>
       </IonHeader>
 
       <IonContent className='cv-content'>
 
 
-
         <IonCard className='cv-card'>
           <IonCardHeader>
-            <IonCardTitle>
-              Desarrollador FullStack
-
+            <IonCardTitle className="cv-card-title-2">
+              Perfil Profesional
             </IonCardTitle>
-            <IonCardSubtitle>
-              Epidata · Jornada completa · En remoto
-            </IonCardSubtitle>
-            <IonCardSubtitle>
-              jun. 2022 - feb. 2025 · 2 años 9 meses
 
-            </IonCardSubtitle>
           </IonCardHeader>
 
           <IonCardContent style={{ textIndent: '2em', whiteSpace: 'pre-line', lineHeight: '2' }}>
             <span style={{ textIndent: '2em', display: 'block' }}>
-
-              Como desarrolladora FullStack, he trabajado en la creación y mantenimiento de aplicaciones web modernas con React y Spring Boot, dentro de una arquitectura de microservicios. {'\n'}
+              Desarrolladora Full Stack con experiencia en el desarrollo de aplicaciones web
+              modernas utilizando Java + Spring Boot en el backend y React/Vue.js en el
+              frontend. He trabajado bajo arquitecturas de microservicios, aplicando patrones
+              en capas, consumo e integración de APIs RESTful y persistencia de datos con
+              JPA/Hibernate.
             </span>
             <span style={{ textIndent: '2em', display: 'block' }}>
-              He desarrollado nuevos componentes en React, mejorando la eficiencia y experiencia de usuario, además de conectar el frontend con APIs REST usando Axios/Fetch. También participé activamente en la migración de una biblioteca de UX de React a Vue.js.
+              En frontend, cuento con experiencia en la creación y optimización de
+              componentes reutilizables, migraciones de librerías y mejora de la experiencia de
+              usuario. Me motiva participar en proyectos que integren ambos mundos (backend
+              y frontend), aplicando buenas prácticas, metodologías ágiles y trabajo en equipo.
             </span>
-
-            <span style={{ textIndent: '2em', display: 'block' }}>
-
-
-              En el backend, diseñé e implementé soluciones en Java 8 y Spring Boot, estructurando controladores, servicios y repositorios, con persistencia en MySQL mediante JPA/Hibernate. {'\n'}
-            </span>
-            <span style={{ textIndent: '2em', display: 'block' }}>
-
-              Además, cuento con experiencia en control de versiones con Git (GitHub/GitLab) y he trabajado bajo metodologías ágiles, utilizando Jira para la gestión de tareas y proyectos. {'\n'}
-            </span>
-
-            <IonList>
-              <IonItem>
-                <IonLabel>Java 8 - JPA - Hibernate - MySQL - Spring Boot - Microservicios - Maven</IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>JavaScript - TypeScript - React 18 - HTML - CSS - Material UI v5</IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>Scrum - JIRA - Figma -  Git/GitLab </IonLabel>
-              </IonItem>
-            </IonList>
 
           </IonCardContent>
 
         </IonCard>
 
+
         <IonCard className='cv-card'>
           <IonCardHeader>
-            <IonCardTitle>
-              Desarrollador de Back-End Trainee
+            <IonCardTitle className="cv-card-title-2">
+              Experiencia Profesional
             </IonCardTitle>
-            <IonCardSubtitle>
-              PinApp · Contrato de formación · Media jornada · Hibrido
+            <IonCardSubtitle className="cv-card-title-3">
+              Desarrolladora Full Stack Junior | Epidata
             </IonCardSubtitle>
-            <IonCardSubtitle>
-              nov. 2021 - dic. 2021 · 2 meses
+            <IonCardSubtitle className="cv-card-title-3">
+              Remoto | jun. 2022 – feb. 2025 (2 años 9 meses)
             </IonCardSubtitle>
           </IonCardHeader>
 
           <IonCardContent style={{ textIndent: '2em', whiteSpace: 'pre-line', lineHeight: '2' }}>
             <span style={{ textIndent: '2em', display: 'block' }}>
 
-              Colaboración en el desarrollo de un carrito de compras.
-            </span>
+              • Desarrollo y mantenimiento de aplicaciones web bajo arquitectura de
+              microservicios.            </span>
             <span style={{ textIndent: '2em', display: 'block' }}>
-              Desarrollé el microservicio generador de pedidos en Java con Spring Boot, estructurado bajo una arquitectura en capas: controladores, servicios, repositorios y modelos.             </span>
+              • Backend: implementación de servicios en Java 8 + Spring Boot, persistencia con
+              MySQL (JPA/Hibernate) y pruebas con JUnit/Mockito.            </span>
 
             <span style={{ textIndent: '2em', display: 'block' }}>
-
-
-              Utilicé Hibernate como ORM para la persistencia y el mapeo en MySQL, con consultas a través de JPA. Implementé conexiones a otros microservicios para consultar datos sobre el stock y los clientes, con el fin de conformar el pedido.             </span>
+              • Frontend: creación de componentes en React 18 (Material UI) y colaboración en
+              la migración de librerías a Vue.js, mejorando eficiencia y experiencia de usuario.       </span>
             <span style={{ textIndent: '2em', display: 'block' }}>
 
-              Gestión de versiones mediante Git/GitHub y uso de la metodología ágil Scrum como dinámica de desarrollo, con tableros en Trello.            </span>
+              • Uso de GitHub/GitLab, Jira y metodologías ágiles Scrum en equipos distribuidos.            </span>
 
             <IonList>
               <IonItem>
-                <IonLabel>Java 7 y 8 - JPA - Hibernate  - Spring Boot - Microservicios - Maven</IonLabel>
+                <IonLabel>Stack: Java 8 · Spring Boot · Microservicios · APIs REST · Maven · Hibernate · JPA · MySQL</IonLabel>
               </IonItem>
               <IonItem>
-                <IonLabel>H2 - MySQL - Mockito - JUnit - Postman - API de Swagger
+                <IonLabel>  React 18 ·  HTML · CSS · JavaScript · TypeScript · Material UI · Vue.js </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel> GitHub/GitLab · Jira  · Figma · Scrum </IonLabel>
+              </IonItem>
+            </IonList>
+
+          </IonCardContent>
+
+
+          <IonCardHeader>
+
+            <IonCardSubtitle className="cv-card-title-3">
+              Desarrolladora Backend Trainee | PinApp            </IonCardSubtitle>
+            <IonCardSubtitle className="cv-card-title-3">
+              Córdoba (Híbrido) | nov. 2021 – dic. 2021 (2 meses)            </IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent style={{ textIndent: '2em', whiteSpace: 'pre-line', lineHeight: '2' }}>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Desarrollo de un microservicio de gestión de pedidos en Java con Spring Boot,
+              bajo arquitectura en capas.
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Persistencia con Hibernate/JPA en MySQL e integración con microservicios para
+              stock y clientes.
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Trabajo en equipo ágil bajo Scrum, con GitHub, Swagger, Postman y Trello.
+            </span>
+
+            <IonList>
+              <IonItem>
+                <IonLabel>Stack: Java 7/8 · Spring Boot · JPA · Hibernate · MySQL · GitHub · Swagger</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Postman · JUnit · Mockito · Maven · Trello
                 </IonLabel>
               </IonItem>
-              <IonItem>
-                <IonLabel>Scrum - Trello -  Git/GitLab </IonLabel>
-              </IonItem>
             </IonList>
 
           </IonCardContent>
 
         </IonCard>
 
+
+        <IonCard className='cv-card'>
+          <IonCardHeader>
+            <IonCardTitle className="cv-card-title-2">
+              Formación Académica            </IonCardTitle>
+
+          </IonCardHeader>
+
+          <IonCardContent style={{ textIndent: '2em', whiteSpace: 'pre-line', lineHeight: '2' }}>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Ingeniería en Computación - Universidad Nacional de Córdoba – FCEFyN
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Algoritmos y estructuras de datos · Programación orientada a objetos ·
+              Concurrente
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Desarrollo en Java, C, C++, ASM, Verilog
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              • Bases de datos · Protocolos de red · Sistemas Linux/Windows
+            </span>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Curso Intensivo de Inglés – Nivel 4 (feb. 2023 – abr. 2023)
+              Universidad Nacional de Córdoba – Facultad de Lenguas
+            </span>
+          </IonCardContent>
+
+        </IonCard>
+
+        <IonCard className='cv-card'>
+          <IonCardHeader>
+            <IonCardTitle className="cv-card-title-2">
+              Habilidades Técnicas
+            </IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent style={{ textIndent: '2em', whiteSpace: 'pre-line', lineHeight: '2' }}>
+            <IonCardSubtitle className="cv-card-title-3">
+              Backend:
+            </IonCardSubtitle>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Java · Spring Boot · JPA · Hibernate · MySQL · H2            </span>
+            <IonCardSubtitle className="cv-card-title-3">
+              Frontend:
+            </IonCardSubtitle>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              React 18 · Vue.js · JavaScript · TypeScript · HTML · CSS · Material UI     </span>
+            <IonCardSubtitle className="cv-card-title-3">
+              Herramientas & Testing:
+            </IonCardSubtitle>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Git · GitHub · GitLab · Postman · Swagger · JUnit · Mockito
+              · Maven · Figma            </span>
+            <IonCardSubtitle className="cv-card-title-3">
+              Metodologías:
+            </IonCardSubtitle>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Metodologías: Scrum · Jira · Trello            </span>
+            <IonCardSubtitle className="cv-card-title-3">
+              Otros:
+            </IonCardSubtitle>
+            <span style={{ textIndent: '2em', display: 'block' }}>
+              Arquitectura en microservicios · APIs RESTful
+            </span>
+          </IonCardContent>
+
+        </IonCard>
 
       </IonContent>
 
